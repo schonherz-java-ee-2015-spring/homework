0ec20a0ef410ac574f952231cf3fb07a68446258
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-02-29 13:37:34 +0100

    Delete unnecessary files

--- IvanyiNagyGabor/HomeWork1/src/hu/schonherz/java/training/homwork1/filereader/Readable.java
+++
@@ -1,9 +0,0 @@
-package hu.schonherz.java.training.homwork1.filereader;
-
-import java.io.File;
-import java.util.List;
-
-@FunctionalInterface
-public interface Readable {
-	public abstract List<? extends Object> readFromFile(File file);
-}
