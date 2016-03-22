df3b6bcb98093b079f1ea30e89e0d6a91d190fb0
Kovács Szabolcs <montuV9@gmail.com>
2016-03-22 13:22:30 +0100

    homework4

---
+++ KovacsSzabolcsZoltan/Homework4/JDBCHomework/src/hu/schonherz/training/classes/User.java
@@ -0,0 +1,40 @@
+package hu.schonherz.training.classes;
+
+public class User {
+	
+	private String name;
+	private int id;
+	
+	public User(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ KovacsSzabolcsZoltan/Homework4/JDBCHomework/src/hu/schonherz/training/dao/ProductDao.java
@@ -0,0 +1,19 @@
+package hu.schonherz.training.dao;
+
+import java.util.List;
+
+import hu.schonherz.training.classes.Product;
+
+public interface ProductDao {
+
+	public List<Product> getAllProducts();
+
+	public Product getProduct(String name);
+	
+	public void addProduct(Product product);
+
+	public void updateProduct(Product product);
+
+	public void deleteProduct(Product product);
+
+}
