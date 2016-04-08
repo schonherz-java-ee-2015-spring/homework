fe0351a1e8766ddd30d3e062d97c20109d234471
Magyari Norbert <magyari.mn@gmail.com>
2016-04-08 23:59:55 +0200

    JSF homework without multitier - not working

---
+++ MagyariNorbert/HW_07/JSFHomework2/src/main/java/jsf/dao/LoginDAO.java
@@ -0,0 +1,36 @@
+package jsf.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+ 
+import jsf.util.DataConnect;
+ 
+public class LoginDAO {
+ 
+    public static boolean validate(String user, String password) {
+        Connection con = null;
+        PreparedStatement ps = null;
+ 
+        try {
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("Select userName, password from public.User where userName = ? and password = ?");
+            ps.setString(1, user);
+            ps.setString(2, password);
+ 
+            ResultSet rs = ps.executeQuery();
+ 
+            if (rs.next()) {
+                //result found, means valid inputs
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Login error -->" + ex.getMessage());
+            return false;
+        } finally {
+            DataConnect.close(con);
+        }
+        return false;
+    }
+}
---
+++ MagyariNorbert/HW_07/JSFHomework2/src/main/java/jsf/util/DataConnect.java
@@ -0,0 +1,27 @@
+package jsf.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class DataConnect {
+ 
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/JSFHomework", "postgres", "xcz4444T");
+            return con;
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->"
+                    + ex.getMessage());
+            return null;
+        }
+    }
+ 
+    public static void close(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
+}
